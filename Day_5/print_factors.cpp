e1a58a28871d92ffbc4a9f10ae50ba3787148518
Pratishtha Mishra <mpratishtha3@gmail.com>
2026-06-05 23:57:38 +0530

    Printing factors

--- Day_5/print_factors.cpp
+++ Day_5/print_factors.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 int main()
 {
-    int
+    int n;
+    int sum=0,temp=0;
+    cout<<"Give a number";
+    cin>>n;
+    int num;
+    num=n;
+    for(int i=1;i<num;i++)
+    {
+        temp=num%i;
+        if(temp==0)
+        {
+             cout<<i<<endl;
+        }
+    }
 }
