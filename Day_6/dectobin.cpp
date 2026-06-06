080fefb682c1f647b1aa35235e04d234c5acafee
Pratishtha Mishra <mpratishtha3@gmail.com>
2026-06-07 00:06:42 +0530

    binary to decimal

---
+++ Day_6/dectobin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Give a number";
+    cin>>n;
+    bitset<16>binary(n);
+    cout<<"Binary"<<binary<<endl;
+    return 0;
+}
