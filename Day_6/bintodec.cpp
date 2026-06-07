9769b905099b29ff50e4f79c2dd546d49ea8e685
Pratishtha Mishra <mpratishtha3@gmail.com>
2026-06-07 16:16:44 +0530

    binary to decimal

--- Day_6/bintodec.cpp
+++ Day_6/bintodec.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+int main()
+{
+    int rem,base=1,deci=0,bin;
+    cout<<"Give a binary number:";
+    cin>>bin;
+    while(bin!=0)
+    {
+        rem=bin%10;
+        deci=deci+rem*base;
+        bin/=10;
+        base*=2;
+    }
+    cout<<"Decimal number is:"<<deci<<endl;
+    return 0;
+}
