b70c3869f9bb5a03273e928f4aec63e97b4c6cbc
Pratishtha Mishra <mpratishtha3@gmail.com>
2026-06-08 22:00:57 +0530

    removed if else statement for efficiency

--- Day_8/number_triangle.cpp
+++ Day_8/number_triangle.cpp
@@ -7,26 +7,7 @@ int main()
     {
         for(j=0;j<=i;j++)
         {
-            if(j==0)
-            {
-                cout<<"1";
-            }
-            else if(j==1)
-            {
-                cout<<"2";
-            }
-            else if(j==2)
-            {
-                cout<<"3";
-            }
-            else if(j==3)
-            {
-                cout<<"4";
-            }
-            else if(j==4)
-            {
-                cout<<"5";
-            }
+            cout<<j+1;
         }
         cout<<endl;
 
