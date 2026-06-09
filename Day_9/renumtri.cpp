a9c086689aa53bca17413dc70a0fbfee1eccdbff
Pratishtha Mishra <mpratishtha3@gmail.com>
2026-06-09 22:09:03 +0530

    reverse number triangle

---
+++ Day_9/renumtri.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=4;i>=0;i--)
+    {
+        for(j=0;j<=i;j++)
+        {
+            cout<<j+1;
+        }
+        cout<<endl;
+
+    }
+    
+}
