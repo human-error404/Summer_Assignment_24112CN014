11c3f46a0801d11b130c9e58aefc2f6554dc28c2
Pratishtha Mishra <mpratishtha3@gmail.com>
2026-06-27 14:25:20 +0530

    I want the world in my hands

---
+++ Day_19/subtract.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m,n;
+    cout<<"Give the number of rows in matrix A and B:";
+    cin>>m;
+    cout<<"Give the number of coloumns in matrix A and B:";
+    cin>>n;
+    int A[m][n];
+    int B[m][n];
+    int sum[m][n];
+
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<"give the value for A["<<i<<"]"<<"["<<j<<"]";
+            cin>>A[i][j];
+        }
+    }
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<"give the value for B["<<i<<"]"<<"["<<j<<"]";
+            cin>>B[i][j];
+        }
+    }
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            sum[i][j]=A[i][j]-B[i][j];
+        }
+    }
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<sum[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
